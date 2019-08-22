d0717ea6389d9f57e1e25399d28e61ccaf89a206
aconanec <alexandreconanec@gmail.com>
2019-08-22 11:04:31 +0200

    add the possibility to have only the front pareto plot. Add also the return of the stability indicators when asking the full detail of the stability of convergence

--- R/plot_simu.R
+++ R/plot_simu.R
@@ -1,4 +1,8 @@
-plot.simu <- function(simu, f_studied=2, f_view=1, solution_space, breaks=100){
+plot.simu <- function(simu, f_studied=2, f_view=1, solution_space, breaks=100,
+                      choice = "all"){
+
+    if (!choice %in% c("all", "sol_space"))
+        stop("choice must be in c(\"all\", \"sol_space\")")
 
     res <- simu$res
     colnames(solution_space)[c(f_view, f_studied)] = c("x", "y")
@@ -23,37 +27,49 @@ plot.simu <- function(simu, f_studied=2, f_view=1, solution_space, breaks=100){
         ylab(paste0("f", f_studied))
 
     p2 <- p1 + xlim(min(res_tot$f_view), max(res_tot$f_view)) +
-        theme(legend.position='none',
-              plot.margin = unit(c(1,0,1,0), "cm"))
+        theme(legend.position='none')
 
-    p3 <- ggplot(data = front_variability) +
-        geom_bar(aes(x = df, y=sigma), stat = "identity") +
-        theme(axis.text.x = element_blank(),
-              axis.ticks.x = element_blank(),
-              axis.title.x = element_blank(),
-              plot.margin = unit(c(1,1,1,1), "cm"))
+    if (choice == "all"){
+        p3 <- ggplot(data = front_variability) +
+            geom_bar(aes(x = df, y=sigma), stat = "identity") +
+            theme(axis.text.x = element_blank(),
+                  axis.ticks.x = element_blank(),
+                  axis.title.x = element_blank(),
+                  plot.margin = unit(c(1,1,1,1), "cm"))
 
-    p4 <- ggplot(data = front_variability)  +
-        geom_line(aes(x=df, y=n, group=1)) +
-        theme(axis.text.x = element_blank(),
-              axis.ticks.x = element_blank(),
-              axis.title.x = element_blank(),
-              plot.margin = unit(c(1,1,1,1), "cm"))
+        p4 <- ggplot(data = front_variability)  +
+            geom_line(aes(x=df, y=n, group=1)) +
+            theme(axis.text.x = element_blank(),
+                  axis.ticks.x = element_blank(),
+                  axis.title.x = element_blank(),
+                  plot.margin = unit(c(1,1,1,1), "cm"))
 
-    figure <- ggarrange(p2, p3, p4,
-                        labels = c("Front Pareto", "Variabilté du front",
-                                   "Nombre de points par unité de f1"),
-                        ncol = 1, nrow = 3)
+        figure <- ggarrange(p2 + theme(plot.margin = unit(c(1,0,1,0), "cm")),
+                                       p3, p4,
+                            labels = c("Front Pareto", "Variabilt\u00e9 du front",
+                                       "Nombre de points par unit\u00e9 de f1"),
+                            ncol = 1, nrow = 3)
 
-    evaluation <- paste0("Variabilite moyenne = ",
-                         mean(front_variability$sigma, na.rm = T) %>% round(1),
-                         "                        Variabilite ecart type = ",
-                         sd(front_variability$sigma, na.rm = T) %>% round(1))
+        evaluation <- paste0("Variabilite moyenne = ",
+                             mean(front_variability$sigma, na.rm = T) %>% round(1),
+                             "                        Variabilite ecart type = ",
+                             sd(front_variability$sigma, na.rm = T) %>% round(1))
 
-    annotate_figure(figure,
-                    top = text_grob(paste0("B = ", simu$config$B), color = "red",
-                                    face = "bold", size = 14),
-                    bottom = text_grob(evaluation,
-                                       color = "blue", size = 11))
+        plot_res <- annotate_figure(figure,
+                        top = text_grob(paste0("B = ", simu$config$B), color = "red",
+                                        face = "bold", size = 14),
+                        bottom = text_grob(evaluation,
+                                           color = "blue", size = 11))
+
+        list(plot = plot_res,
+             var_mean = mean(front_variability$sigma, na.rm = T) %>% round(3),
+             var_sd = sd(front_variability$sigma, na.rm = T) %>% round(3))
+
+    }else if (choice == "sol_space"){
+        annotate_figure(p2,
+                        top = text_grob(paste0("B = ", simu$config$B), color = "red",
+                                        face = "bold", size = 14)
+        )
+    }
 
 }
