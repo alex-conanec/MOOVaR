99408d4d4c6906a3566cab2b9f063af94976871d
aconanec <alexandreconanec@gmail.com>
2019-09-12 18:03:51 +0200

    fix the bug of translating the sens into comparison sign. Add the posibility to compare on only one function. Externalize the comparison in another function

--- R/pairwise_comparaison.R
+++ R/pairwise_comparaison.R
@@ -14,62 +14,35 @@
 #'
 #' @examples
 #' sum(1:10)
+#' @export
 
 pairwise_comparison <- function(X, sens){
 
     if (any(!sens %in% c("min", "max"))) stop("sens must be either 'min' or 'max'")
-    dom_sens <- c('>', '<')[as.factor(sens=="min")]
+    dom_sens <- sens
+    dom_sens[which(sens == "min")] <- '<'
+    dom_sens[which(sens == "max")] <- '>'
     res <- list()
 
+    if (class(X) == "numeric") X <- data.frame(X)
     for (k in 1:NROW(X)){
         res[[as.character(k)]]$dominated_count <- 0
         res[[as.character(k)]]$dominating_index <- NULL
 
         for (i in (1:NROW(X))[-k]){
-            comparison <- sapply(1:NCOL(X), function(j){
-                outer(X[k,j], X[i,j], dom_sens[j])
-            })
 
-            equals <- sapply(1:NCOL(X), function(j){
-                outer(X[k,j], X[i,j], '==')
-            })
+            comp <- comparaison(X1 = X[k,],
+                                X2 = X[i,],
+                                sens = sens)
 
-            if (!(any(comparison) & any(!comparison)) & !any(equals)){ #comparable
-                if (!all(comparison)){
-                    res[[k]]$dominated_count <- res[[k]]$dominated_count + 1
-                }else if (all(comparison)){
+            if (!is.null(comp)){
+                if (comp){
                     res[[k]]$dominating_index <- c(res[[k]]$dominating_index, i)
+                }else{
+                    res[[k]]$dominated_count <- res[[k]]$dominated_count + 1
                 }
             }
         }
     }
     res
 }
-
-# pairwise_comparison <- function(X, sens){
-# require(tidyverse)
-#     dom_sens <- c('<', '>')[as.factor(sens=="min")]
-#
-#     result <- lapply(1:NROW(X), function(k){
-#         res <- list(dominated_count = 0, dominating_index = NULL)
-#
-#         comparisons <- lapply((1:NROW(X))[-k], function(i){
-#             sapply(1:NCOL(X), function(j){
-#                 outer(X[k,j], X[i,j], dom_sens[j])
-#             })
-#         })
-#
-#         res$dominated_count <-
-#             sapply(comparisons, function(comparison){
-#                 all(!comparison)
-#             }) %>% which() %>% length()
-#
-#         res$dominating_index <-
-#             sapply(1:length(comparisons), function(i){
-#                 if (all(comparisons[[i]])) (1:NROW(X))[-k][i]
-#             }) %>% unlist()
-#
-#         res
-#     })
-#     result
-# }
