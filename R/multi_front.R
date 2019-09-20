2226dd16a1579c79bf1b80183a3215398aae3284
aconanec <alexandreconanec@gmail.com>
2019-09-20 12:22:47 +0200

    fix bug in the function of lm prediction, we must first reconsistuate Xreg with X and then make the matrix product with beta. Also fix the calculation of the mse. The p used must be the one of Xreg and not X.

--- R/multi_front.R
+++ R/multi_front.R
@@ -18,54 +18,14 @@ multi_front <- function(NSGA_param, model, type = "bootstrap", B = 100){
         })
     }else if (type == "var_param"){
 
-        betas <- lapply(seq_along(model$param), function(j){
-            reg <- lm(model$param[[j]]$formule,
-                      data = cbind(Y = model$Y_train[,j], model$X_train))
-
-            #sce & mse
-            sce <- (model$Y_train[,j] - reg$fitted.values)^2 %>% sum()
-            p <- NCOL(model$X_train)
-            mse <- (1/(n - p - 1)) * sce
-
-            # deal with formulas and the matrix
-            var_reg <- model$param[[j]]$formule %>%
-                stringr::str_extract(pattern = "(?<=~).*") %>%
-                strsplit(split = "+", fixed = T) %>% unlist() %>%
-                strsplit(split = ":")
-
-            X_reg <- sapply(var_reg, function(var){
-                if (length(var) == 1){
-                    without_space <- strsplit(var, split = " ")[[1]]
-                    X[, without_space[! without_space %in% ""]]
-                }else{
-                    sapply(var, function(v){
-                        without_space <- strsplit(v, split = " ")[[1]]
-                        X[, without_space[! without_space %in% ""]]
-                    }) %>% apply(1, prod)
-                }
-            })
-
-            X_reg <- cbind(1, X_reg)
-
-            # loi MVN
-            mvtnorm::rmvnorm(n = B,
-                             mean = reg$coefficients,
-                             sigma = mse * solve(t(X_reg)%*%X_reg))
-
-        })
-
-        model_list <- lapply(seq_len(B), function(i){
-            lapply(seq_along(model$param), function(j){
-                model <- function(X, beta){
-                    as.matrix(X) %*% beta
-                }
-                formals(model)$beta <- betas[[j]][i,]
-                model
-            })
-        })
+        formules <- sapply(model$param, function(param) param$formule)
+        model_list <- var_param(formules,
+                                X_train = model$X_train,
+                                Y_train = model$Y_train,
+                                B = B)
     }
 
-    model=model_list[[1]]
+
     mclapply(model_list, function(model){
         for (j in seq_along(model)){
             NSGA_param$ff[[j]]$f <- model[[j]]
@@ -88,3 +48,82 @@ generate_model <- function(type = model$type, X_train = X_train,
         }
     )
 }
+
+#' @export
+var_param <- function(formules, X_train, Y_train, B = 50){
+
+    reg_param <- lapply(seq_along(formules), function(j){
+        reg <- lm(formules[[j]], data = cbind(Y = Y_train[,j], X_train))
+
+        # deal with formulas and the matrix
+        var_reg <- formules[[j]] %>%
+            stringr::str_extract(pattern = "(?<=~).*") %>%
+            strsplit(split = "+", fixed = T) %>% unlist() %>%
+            strsplit(split = ":")
+
+        var_reg <- lapply(var_reg, function(var){
+            if (length(var) == 1){
+                without_space <- strsplit(var, split = " ")[[1]]
+                without_space[! without_space %in% ""]
+            }else{
+                sapply(var, function(v){
+                    without_space <- strsplit(v, split = " ")[[1]]
+                    without_space[! without_space %in% ""]
+                })
+            }
+        })
+
+        X_reg <- sapply(var_reg, function(var){
+            if (length(var) == 1){
+                X_train[, var]
+            }else{
+                sapply(var, function(v){
+                    X_train[, v]
+                }) %>% apply(1, prod)
+            }
+        })
+
+        X_reg <- cbind(1, X_reg)
+
+
+        #sce & mse
+        sce <- (Y_train[,j] - reg$fitted.values)^2 %>% sum()
+        p <- NCOL(X_reg); n <- NROW(X_reg)
+        mse <- (1/(n - p - 1)) * sce
+
+        # loi MVN
+        beta <- mvtnorm::rmvnorm(n = B,
+                         mean = reg$coefficients,
+                         sigma = mse * solve(t(X_reg)%*%X_reg))
+
+        list(beta = beta, var_reg = var_reg)
+    })
+
+    lapply(seq_len(B), function(i){
+        lapply(seq_along(formules), function(j){
+            model <- function(X, beta, var_reg){
+
+                X_reg <- sapply(var_reg, function(var){
+                    if (length(var) == 1){
+                        X[, var]
+                    }else{
+                        sapply(var, function(v){
+                            X[, v]
+                        }) %>% apply(1, prod)
+                    }
+                })
+
+                X_reg <- cbind(1, X_reg)
+
+                as.matrix(X_reg) %*% beta
+            }
+            formals(model)$beta <- reg_param[[j]]$beta[i,]
+            formals(model)$var_reg <- reg_param[[j]]$var_reg
+            model
+        })
+    })
+}
+
+
+
+
