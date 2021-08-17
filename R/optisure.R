8a2afc2a9fd970e560e8ccba56e51efa23a31aaf
aconanec <alexandreconanec@gmail.com>
2021-08-17 17:05:51 +0200

    add the updateProgress function. some modif about the arg in the NSGA call. Modif to allow user to use global and individual quantile

--- R/optisure.R
+++ R/optisure.R
@@ -33,7 +33,7 @@
 #' }) %>% as.data.frame(row.names = seq_len(n))
 #'
 #' # Opti
-#' res = optisure(X, Y, N=20, B=10, alpha = 0.5)
+#' res = optisure(X, Y, N=20, TT=10, alpha = 0.5)
 #'
 #' plot(ellipse_var(Y$Y1, Y$Y2, alpha = 0.05), type = 'l', xlab = "Y1", ylab = "Y2",
 #'      xlim = c(-8,8), ylim = c(-10,6))
@@ -66,7 +66,7 @@
 #' names(fn) = paste0("Y", seq_len(p))
 #'
 #' #opti
-#' res = optisure(X, Y, N=20, B=10, alpha = 0.5)
+#' res = optisure(X, Y, N=20, TT=10, alpha = 0.5)
 #'
 #' plot(ellipse_var(Y$Y1, Y$Y2, alpha = 0.05), type = 'l', xlab = "Y1", ylab = "Y2",
 #'      xlim = c(-8,8), ylim = c(-10,6))
@@ -75,82 +75,128 @@
 #'
 #' @importFrom magrittr %>%
 #' @export
-optisure <- function(X, Y, sens = rep("min", NCOL(Y)),
-                     utility_risk = rep("quantile", NCOL(Y)),
+optisure <- function(X, Y, sens = rep("max", NCOL(Y)),
+                     quantile_utility_idx = rep(TRUE, NCOL(Y)),
                      g = NULL, X_space_csrt = TRUE,
-                     tau = 0.5, globale_tau = FALSE, reg_method = "linear", #rajouté
-                     # parametric = FALSE, deg = 3, #a virer
-                     N = NULL, B = NULL, seed = NULL,
-                     ...){
+                     tau = rep(0.5, NCOL(Y)),
+                     globale_tau = rep(F, NCOL(Y)),
+                     alpha = 0.15,
+                     # reg_method = "linear", #rajouté
+                     N = NULL, TT = NULL, seed = NULL,
+                     updateProgress = NULL, ...){
 
-  library(reticulate)
+  # library(reticulate)
   d = NCOL(X)
   p = NCOL(Y)
   n = NROW(X)
   col_names_Y = colnames(Y)
+  for (i in which(!sapply(X, is.numeric))) X[,i] = as.factor(X[,i,drop=TRUE])
 
   #sens optimisation
   Y = as.data.frame(Y)
-  # Y = Y * sapply(sens, function(x) ifelse(x == "min", -1, 1))
   Y = sweep(Y, 2, sapply(sens, function(x) ifelse(x == "min", -1, 1)), "*")
 
-  quantile_utility_idx = utility_risk == rep("quantile", NCOL(Y))
+  if (is.function(updateProgress)) {
+    updateProgress(detail = "Training model")
+  }
 
   if (any(quantile_utility_idx)){
-    if (globale_tau){
+    if (!any(quantile_utility_idx[!globale_tau])){#global risk management only
+      glob_tau = unique(tau[quantile_utility_idx])
+      if (length(glob_tau) > 1){
+        stop("tau must be the same for objectif treating in globale risk")
+      }
       qrgr = quant_reg_global_risk(X = X, Y[,quantile_utility_idx, drop = FALSE],
-                                   tau = tau, reg_method = reg_method) #pas sur de la syntax pour transmettre reg_method
+                                   tau = glob_tau)
       m_quantile = qrgr$m
-    }else{
-      m_quantile = fit_model(X, Y=Y[,quantile_utility_idx, drop = FALSE],
-                             tau = tau, reg_method = reg_method,
-                             method = "quantile", ...)
-      # m_quantile = fit_model(X, Y=Y[,quantile_utility_idx, drop = FALSE],
-      #                        tau = tau, reg_method = reg_method,
-      #                        method = "quantile", penalty = rep(0,p))
+    }else if (!any(quantile_utility_idx[globale_tau])){#individual risk management only
+      m_quantile = fit_model(X, Y = Y[,quantile_utility_idx, drop = FALSE],
+                             tau = tau[quantile_utility_idx],
+                             method = "quantile")
+    }else{#gobal AND individual risk management
+      #global risk
+      global_risk_idx = quantile_utility_idx & globale_tau
+      glob_tau = unique(tau[global_risk_idx])
+      if (length(glob_tau) > 1){
+        stop("tau must be the same for objectif treating in globale risk")
+      }
+      qrgr = quant_reg_global_risk(X = X, Y = Y[,global_risk_idx, drop = FALSE],
+                                   tau = glob_tau)
+      m_quantile_glob = qrgr$m
+
+      #individual risk
+      ind_risk_idx = quantile_utility_idx & !globale_tau
+      m_quantile_ind = fit_model(X, Y = Y[,ind_risk_idx, drop = FALSE],
+                                 tau = tau[ind_risk_idx],
+                                 method = "quantile")
     }
   }
 
   if (any(!quantile_utility_idx)){
     m_expect = fit_model(X, Y = Y[,!quantile_utility_idx, drop = FALSE],
-                         reg_method = reg_method, method = "expected")
+                         method = "expected")
   }
 
   if (all(quantile_utility_idx)){
-    m = m_quantile
+    if (any(quantile_utility_idx[globale_tau]) & any(!quantile_utility_idx[globale_tau])){
+      global_quantile = quantile_utility_idx & globale_tau
+      m = function(X){
+        res = cbind(m_quantile_glob(X), m_quantile_ind(X))[, c(which(global_quantile),
+                                                               which(!global_quantile))]
+        colnames(res) = colnames(Y)
+        res
+      }
+    }else{
+      m = m_quantile
+    }
   }else if (all(!quantile_utility_idx)) {
     m = m_expect
   }else{
-    m = function(X){
-      res = cbind(m_quantile(X), m_expect(X))[, c(which(quantile_utility_idx),
-                                                  which(!quantile_utility_idx))]
-      colnames(res) = colnames(Y)
-      res
+    if (any(quantile_utility_idx[globale_tau]) & any(quantile_utility_idx[!globale_tau])){
+      global_quantile = quantile_utility_idx & globale_tau
+      ind_quantile = quantile_utility_idx & !globale_tau
+
+      m = function(X){
+        res = cbind(m_quantile_glob(X), m_quantile_ind(X), m_expect(X))[
+          , c(which(global_quantile),
+              which(ind_quantile),
+              which(!quantile_utility_idx))]
+        colnames(res) = colnames(Y)
+        res
+      }
+    }else{
+      m = function(X){
+        res = cbind(m_quantile(X), m_expect(X))[, c(which(quantile_utility_idx),
+                                                    which(!quantile_utility_idx))]
+        colnames(res) = colnames(Y)
+        res
+      }
     }
+
   }
 
 
   #tunage nsga parametres
-  if (is.null(B)) B = 20
-  if (is.null(N)) N = NROW(X)
+  if (is.null(TT)) TT = 20
+  if (is.null(N)) N = round(NROW(X)/2)
 
   if (X_space_csrt){
+    if (is.function(updateProgress)) {
+      updateProgress(detail = "Decision constraint training")
+    }
     cat("Train belonging to density constraint \n")
-    g$cstr_X_space = def_cstr_X_space(X)$g
+    g$cstr_X_space = def_cstr_X_space(X, alpha = alpha)$g
   }
 
+  # print(g)
 
-  #appelle de NSGA ou optim si NCOL(Y)==1
-  # res = NSGA(X = X, fn = m, n_objective = p, g=g,
-  #            sens = rep("max", p),
-  #            N = N, seed = seed,
-  #            B = B, verbose = TRUE)
-
+  #appelle de NSGA
   res = NSGA(
     X = X,
     fn = m,
     n_objective = p,
     sens = rep("max", p),
+    g = g,
 
     #k tournois
     k_tournament = 10,
@@ -166,22 +212,27 @@ optisure <- function(X, Y, sens = rep("min", NCOL(Y)),
 
     #budget
     N = N,
-    TT = B,
+    TT = TT,
 
     #verbose
     verbose = TRUE,
     front_tracking = TRUE,
-    seed = seed
+    seed = seed,
+    updateProgress = updateProgress
   )
 
-  #mise en forme des resultats
-  if (globale_tau){
-    res$tau_j = qrgr$tau
-    res$globale_confiance = qrgr$globale_confiance
-  }else{
-    res$globale_confiance = sum(apply(Y - m(X) > 0, 1, all))/n
-    res$tau_j = rep(tau, p)
+  if (is.function(updateProgress)) {
+    updateProgress(detail = paste0("end"))
   }
+
+  #mise en forme des resultats ##### a modifier aussi
+  # if (globale_tau){
+  #   res$tau_j = qrgr$tau
+  #   res$globale_confiance = qrgr$globale_confiance
+  # }else{
+  #   res$globale_confiance = sum(apply(Y - m(X) > 0, 1, all))/n
+  #   res$tau_j = rep(tau, p)
+  # }
 
 
   # Y = Y * sapply(sens, function(x) ifelse(x == "min", -1, 1))
