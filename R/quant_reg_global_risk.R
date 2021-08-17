ddef0ba1fed1d8671214af7a35958515f5b73f8c
aconanec <alexandreconanec@gmail.com>
2021-08-17 17:04:09 +0200

    withdraw the "..." argument since it is no longer used

--- R/quant_reg_global_risk.R
+++ R/quant_reg_global_risk.R
@@ -1,5 +1,5 @@
 #' @export
-quant_reg_global_risk = function(X, Y, tau, floor = 5/NROW(X), max_iter = 20, ...){
+quant_reg_global_risk = function(X, Y, tau, floor = 5/NROW(X), max_iter = 20){
 
   p = NCOL(Y)
   n = NROW(Y)
@@ -11,7 +11,7 @@ quant_reg_global_risk = function(X, Y, tau, floor = 5/NROW(X), max_iter = 20, ..
   }
 
   for (t in seq_len(max_iter)){
-    m = fit_model(X, Y, tau + lambda, method = "quantile", ...)
+    m = fit_model(X, Y, tau = tau + lambda, method = "quantile")
 
     #eval
     delta = Y - m(X)
